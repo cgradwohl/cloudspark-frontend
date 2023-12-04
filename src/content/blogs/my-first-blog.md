fc3e4e4d4e8e57b991543abd47a0e05ce0ef35d7
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-12-03 18:42:21 -0800

    get blogs nav working.

---
+++ src/content/blogs/my-first-blog.md
@@ -0,0 +1,10 @@
+---
+author: Chris Gradwohl
+publicationDate: 2023-12-03T16:53:53.226Z
+title: My First Blog
+tags:
+  - docs
+description: my first blog post with astro
+---
+### HELLO
+hello world
---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const blogs = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    // required
+    description: z.string(),
+    publicationDate: z.date(),
+    title: z.string(),
+    // optional
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  blogs,
+};
