8374ea9862fdb0eb9fa2ed35cce3c961d7830f14
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-01-15 05:37:13 -0800

    update types.

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly COURIER_AUTH_TOKEN: string
   readonly TURSO_URL: string
   readonly TURSO_AUTH_TOKEN: string
+  readonly NODE_ENV: 'development' | 'production'
 }
 
 interface ImportMeta {
